2e39093e533e8e083aa2127402d8842ec2101df3
victorvictoria-maker <victorvictoria0001@gmail.com>
2024-07-05 12:54:27 +0100

    Implemented searching and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,7 +9,6 @@ import {
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
 
-('Fetched data from the server, implemented streaming using suspense and skeleton, moved data fetching to the components that needs it.');
 async function Page() {
   return (
     <main>
@@ -17,14 +16,6 @@ async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
